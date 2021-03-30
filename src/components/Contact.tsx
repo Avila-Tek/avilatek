b987db3c8f56c53e85bfb778a7d8caaa8dbc8e0d
Erika Han <hanerika16@gmail.com>
2021-03-30 10:23:29 -0400

    contact started

---
+++ src/components/Contact.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <section className="mt-32 mb-56 w-full">
+
+      {/* Section details */}
+      <div className="w-7/12 px-12">
+        <h1 className="text-lg md:text-xl xl:text-2xl font-bold">
+          <span className="text-primary-500">Contáctanos</span> para tu próxima{' '}
+          <mark className="inline-block bg-primary-400 bg-opacity-50 pb-3 leading-0">
+            gran idea
+          </mark>
+        </h1>
+        <p className="w-full mt-6 lg:mt-7 text-sm lg:text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna in sit
+          id hac. Consectetur adipiscing elit. Urna in sit id hac.
+        </p>
+      </div>
+
+      
+
+    </section>
+  );
+}
---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,129 @@
+import React, {
+  ChangeEvent,
+  InputHTMLAttributes,
+  DetailedHTMLProps,
+} from 'react';
+
+type InputProps = DetailedHTMLProps<
+  InputHTMLAttributes<
+    HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+  >,
+  HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+> & {
+  placeholder?: string;
+  name: string;
+  type: string;
+  value: string | number | boolean;
+  label?: string;
+  className?: string;
+  extraClass?: string;
+  marginBottom?: boolean;
+  onChange: (
+    event: ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => void;
+  ref?: React.RefObject<any>;
+  children?: Array<React.ReactChild> | React.ReactChild | any;
+};
+
+export default function Input({
+  placeholder,
+  name,
+  type,
+  value,
+  label,
+  onChange,
+  className,
+  extraClass,
+  children,
+  ref = null,
+  marginBottom = true,
+  ...rest
+}: InputProps) {
+  if (type === 'textarea') {
+    return (
+      <div className={`${className} ${marginBottom ? ' mb-4 ' : ' '}`}>
+        {label && (
+          <label
+            htmlFor={name}
+            className={`w-full block mb-1 font-bold text-sm md:text-base text-primary-500`}
+          >
+            {label}
+          </label>
+        )}
+        <textarea
+          ref={ref}
+          name={name}
+          id={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          value={value}
+          rows={3}
+          className={`
+          bg-medium-blue text-font-black rounded-lg p-1 w-full leading-tight outline-none text-sm md:text-base focus:border-2 focus:border-primary-300 disabled:opacity-75
+            ${extraClass}`}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') e.preventDefault();
+          }}
+        />
+      </div>
+    );
+  }
+
+  if (type === 'select') {
+    return (
+      <div className={`${className} ${marginBottom ? ' mb-4 ' : ' '}`}>
+        {label && (
+          <label
+            htmlFor={name}
+            className={`w-full block mb-1 font-bold text-sm md:text-base text-primary-500`}
+          >
+            {label}
+          </label>
+        )}
+        <select
+          ref={ref}
+          name={name}
+          id={name}
+          onChange={onChange}
+          value={value}
+          className={`
+          bg-medium-blue text-font-black rounded-lg p-1 w-full leading-tight outline-none text-sm md:text-base focus:border-2 focus:border-primary-300 disabled:opacity-75
+            ${extraClass}`}
+        >
+          {children}
+        </select>
+      </div>
+    );
+  }
+
+  return (
+    <div className={`${className} ${marginBottom ? ' mb-4 ' : ' '}`}>
+      {label && (
+        <label
+          htmlFor={name}
+          className={`w-full block mb-1 font-bold text-sm md:text-base text-primary-500`}
+        >
+          {label}
+        </label>
+      )}
+      <input
+        ref={ref}
+        name={name}
+        id={name}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+        className={`
+          bg-medium-blue text-font-black rounded-lg p-1 w-full leading-tight outline-none text-sm md:text-base focus:border-2 focus:border-primary-300 disabled:opacity-75
+            ${extraClass}`}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') e.preventDefault();
+        }}
+        {...rest}
+      />
+    </div>
+  );
+}
