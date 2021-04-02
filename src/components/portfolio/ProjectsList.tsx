ef226cf2d189d0ec0881aacf17a2b674721f63ea
Erika Han <hanerika16@gmail.com>
2021-04-02 12:36:39 -0400

    changed idx to project's title

--- src/components/portfolio/ProjectsList.tsx
+++ src/components/portfolio/ProjectsList.tsx
@@ -9,8 +9,8 @@ interface ProjectsListProps {
 export default function ProjectsList({ projects }: ProjectsListProps) {
   return (
     <div className="w-10/12 mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 lg:gap-12 xl:gap-14 pb-40">
-      {projects.map((project, idx) => (
-        <ProjectCard {...project} key={idx} />
+      {projects.map((project) => (
+        <ProjectCard {...project} key={project?.title} />
       ))}
     </div>
   );
