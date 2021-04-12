618e4a8bedc53b496fbd5447c782a2b0543d88f5
Erika Han <hanerika16@gmail.com>
2021-04-12 14:04:18 -0400

    project card dark mode

--- src/components/portfolio/ProjectCard.tsx
+++ src/components/portfolio/ProjectCard.tsx
@@ -19,7 +19,10 @@ export default function ProjectCard({
   link,
 }: ProjectCardProps) {
   return (
-    <div className="group relative h-56 sm:h-60 lg:h-64 w-10/12 sm:w-full max-w-sm mx-auto rounded-lg overflow-hidden shadow-blue dark:shadow-dark-gray">
+    <motion.div
+      whileHover={{ scale: 1.02 }}
+      className="group relative h-56 sm:h-60 lg:h-64 w-10/12 sm:w-full max-w-sm mx-auto rounded-lg overflow-hidden shadow-blue dark:shadow-dark-gray"
+    >
       {/* Image of the project */}
       <div className="w-full h-full overflow-hidden">
         <img
@@ -30,7 +33,7 @@ export default function ProjectCard({
       </div>
 
       {/* Info that appears when you hover */}
-      <div className="w-full h-full absolute top-0 left-0 p-7 bg-primary-100 bg-opacity-80 flex flex-col justify-between opacity-0 group-hover:opacity-100 duration-500 transition-all ease-in-out">
+      <div className="w-full h-full absolute top-0 left-0 p-7 bg-primary-100 dark:bg-primary-500 bg-opacity-80 dark:bg-opacity-80 flex flex-col justify-between opacity-0 group-hover:opacity-100 duration-500 transition-all ease-in-out">
         <div>
           <h2 className="font-bold text-base md:text-lg cursor-default">
             {title}
@@ -47,12 +50,12 @@ export default function ProjectCard({
         <a
           href={link}
           target="_blank"
-          className="flex items-center w-full text-secondary-500 hover:text-primary-500 duration-150 transition-all ease-in-out"
+          className="flex items-center w-full text-secondary-500 dark:text-font-white hover:text-primary-500 dark:hover:text-primary-100 duration-150 transition-all ease-in-out"
         >
           <p className="font-medium text-sm mr-3">Visitar</p>
           <LongArrow className="h-4" />
         </a>
       </div>
-    </div>
+    </motion.div>
   );
 }
