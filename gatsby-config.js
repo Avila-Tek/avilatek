7f737f3b3c64cccfee958e6824f7cf16cd791320
Erika Han <hanerika16@gmail.com>
2021-04-21 14:21:52 -0400

    unused packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,10 @@ module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
     'gatsby-plugin-gatsby-cloud',
-    'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
     'gatsby-transformer-remark',
     'gatsby-plugin-mdx',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
@@ -25,22 +22,6 @@ module.exports = {
       options: {
         icon: 'src/images/icon.png',
       },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'pages',
-        path: './src/pages/',
-      },
-      __key: 'pages',
     },
     {
       resolve: 'gatsby-plugin-crisp-chat',
