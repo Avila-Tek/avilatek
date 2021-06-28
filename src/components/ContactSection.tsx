b61e015e29a0bf0bf7ec3d760bbf88fe8babdc87
Erika Han <hanerika16@gmail.com>
2021-06-27 21:06:47 -0400

    active link when scrolling is working better

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -14,83 +14,83 @@ export default function ContactSection() {
   const language = getCurrentLanguage();
 
   return (
-    <Waypoint
-      onEnter={() => {
-        updateUrl(`/${language === 'es' ? '' : language + '/'}#contact`);
-      }}
+    <section
+      id="contact"
+      className="pt-24 -mt-24 pb-48 w-full px-14 md:px-20 lg:px-28 xl:px-44 relative z-10 overflow-hidden"
     >
-      <section
-        id="contact"
-        className="pt-24 -mt-24 pb-48 w-full px-14 md:px-20 lg:px-28 xl:px-44 relative z-10 overflow-hidden"
-      >
-        <Fade cascade triggerOnce>
-          {/* Section details */}
-          <div className="w-11/12 sm:w-10/12 md:w-9/12 xl:w-6/12 mb-16 mx-auto md:mx-0">
-            <h1 className="text-lg md:text-xl xl:text-2xl font-bold">
-              <span className="text-primary-400">
-                {translation(language, 'contactUs.span')}{' '}
-              </span>{' '}
-              {translation(language, 'contactUs.first')}{' '}
-              <mark className="text-font0-black inline-block bg-primary-400 bg-opacity-50 pb-3 leading-0 dark:text-font-white">
-                {translation(language, 'contactUs.mark')}
-              </mark>
-            </h1>
-            <p className="w-full mt-6 lg:mt-7 text-sm lg:text-base">
-              {translation(language, 'contactUs.text')}
-            </p>
-          </div>
+      <Fade cascade triggerOnce>
+        {/* Section details */}
+        <div className="w-11/12 sm:w-10/12 md:w-9/12 xl:w-6/12 mb-16 mx-auto md:mx-0">
+          <h1 className="text-lg md:text-xl xl:text-2xl font-bold">
+            <span className="text-primary-400">
+              {translation(language, 'contactUs.span')}{' '}
+            </span>{' '}
+            {translation(language, 'contactUs.first')}{' '}
+            <mark className="text-font0-black inline-block bg-primary-400 bg-opacity-50 pb-3 leading-0 dark:text-font-white">
+              {translation(language, 'contactUs.mark')}
+            </mark>
+          </h1>
+          <p className="w-full mt-6 lg:mt-7 text-sm lg:text-base">
+            {translation(language, 'contactUs.text')}
+          </p>
+        </div>
 
-          <div className="w-full flex flex-col lg:flex-row items-center justify-between">
-            {/* Contact form */}
-            <ContactForm />
+        <Waypoint
+          onEnter={() => {
+            updateUrl(`/${language === 'es' ? '' : language + '/'}#contact`);
+          }}
+        />
 
-            {/* Contact us illustration */}
-            <div className="w-10/12 sm:w-7/12 lg:w-5/12 pt-8 lg:pt-0 mt-12 lg:mt-0 mx-auto lg:mx-0 h-full">
-              <CallCenter />
-              {/* Extra info */}
-              <div className="mt-10">
-                {/* Fast response */}
-                <div className="flex items-center">
-                  <LightningIcon className="h-6 w-6 mr-6 text-primary-400" />
-                  <div>
-                    <h2 className="font-bold text-primary-400 text-sm lg:text-base">
-                      {translation(language, 'contactUs.quickResponse')}
-                    </h2>
-                    <p className="mt-1 text-xsm lg:text-sm">
-                      {translation(language, 'contactUs.quickResponseText')}
-                    </p>
-                  </div>
+        <div className="w-full flex flex-col lg:flex-row items-center justify-between">
+          {/* Contact form */}
+          <ContactForm />
+
+          {/* Contact us illustration */}
+          <div className="w-10/12 sm:w-7/12 lg:w-5/12 pt-8 lg:pt-0 mt-12 lg:mt-0 mx-auto lg:mx-0 h-full">
+            <CallCenter />
+            {/* Extra info */}
+            <div className="mt-10">
+              {/* Fast response */}
+              <div className="flex items-center">
+                <LightningIcon className="h-6 w-6 mr-6 text-primary-400" />
+                <div>
+                  <h2 className="font-bold text-primary-400 text-sm lg:text-base">
+                    {translation(language, 'contactUs.quickResponse')}
+                  </h2>
+                  <p className="mt-1 text-xsm lg:text-sm">
+                    {translation(language, 'contactUs.quickResponseText')}
+                  </p>
                 </div>
-                {/* Consulting */}
-                <div className="flex items-center my-4">
-                  <MessageIcon className="h-12 w-12 mr-6 text-primary-400" />
-                  <div>
-                    <h2 className="font-bold text-primary-400 text-sm lg:text-base">
-                      {translation(language, 'contactUs.consulting')}
-                    </h2>
-                    <p className="mt-1 text-xsm lg:text-sm">
-                      {translation(language, 'contactUs.consultingText')}
-                    </p>
-                  </div>
+              </div>
+              {/* Consulting */}
+              <div className="flex items-center my-4">
+                <MessageIcon className="h-12 w-12 mr-6 text-primary-400" />
+                <div>
+                  <h2 className="font-bold text-primary-400 text-sm lg:text-base">
+                    {translation(language, 'contactUs.consulting')}
+                  </h2>
+                  <p className="mt-1 text-xsm lg:text-sm">
+                    {translation(language, 'contactUs.consultingText')}
+                  </p>
                 </div>
-                {/* Personalized */}
-                <div className="flex items-center">
-                  <SettingsIcon className="h-12 w-12 mr-7 text-primary-400" />
-                  <div>
-                    <h2 className="font-bold text-primary-400 text-sm lg:text-base">
-                      {translation(language, 'contactUs.tailorMade')}
-                    </h2>
-                    <p className="mt-1 text-xsm lg:text-sm">
-                      {translation(language, 'contactUs.tailorMadeText')}
-                    </p>
-                  </div>
+              </div>
+              {/* Personalized */}
+              <div className="flex items-center">
+                <SettingsIcon className="h-12 w-12 mr-7 text-primary-400" />
+                <div>
+                  <h2 className="font-bold text-primary-400 text-sm lg:text-base">
+                    {translation(language, 'contactUs.tailorMade')}
+                  </h2>
+                  <p className="mt-1 text-xsm lg:text-sm">
+                    {translation(language, 'contactUs.tailorMadeText')}
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-        </Fade>
-        <div className="absolute top-8 left-1/3 rounded-second-blob w-96 md:w-6/12 h-72 md:h-96 brick bg-medium-blue dark:bg-dark-gray bg-opacity-90 dark:bg-opacity-25" />
-      </section>
-    </Waypoint>
+        </div>
+      </Fade>
+      <div className="absolute top-8 left-1/3 rounded-second-blob w-96 md:w-6/12 h-72 md:h-96 brick bg-medium-blue dark:bg-dark-gray bg-opacity-90 dark:bg-opacity-25" />
+    </section>
   );
 }
--- src/components/services/ServiceSection.tsx
+++ src/components/services/ServiceSection.tsx
@@ -31,44 +31,43 @@ export default function ServiceSection() {
   ];
 
   return (
-    <Waypoint
-      onEnter={() => {
-        updateUrl(`/${language === 'es' ? '' : language + '/'}#services`);
-      }}
+    <section
+      id="services"
+      className="pt-24 md:pt-56 -mt-24 md:-mt-56 w-full pb-48 md:pb-60 flex flex-col md:flex-row px-14 lg:px-24 items-center justify-center relative z-10 overflow-hidden"
     >
-      <section
-        id="services"
-        className="pt-24 md:pt-56 -mt-24 md:-mt-56 w-full pb-48 md:pb-64 flex flex-col md:flex-row px-14 lg:px-24 items-center justify-center relative z-10 overflow-hidden"
-      >
-        <div className="w-10/12 md:w-4/12 mb-20 md:mb-0 mr-0 md:mr-20 xl:mr-28 -mt-6 sm:-mt-10 md:-mt-14 lg:-mt-20">
-          <Fade cascade triggerOnce>
-            <h1 className="text-lg md:text-xl xl:text-2xl font-bold">
-              {translation(language, 'services.first')}{' '}
-              <span className="text-primary-400">
-                {translation(language, 'services.span')}
-              </span>{' '}
-              {translation(language, 'services.second')}{' '}
-              <mark className="text-font0-black inline-block bg-primary-200 bg-opacity-50 pb-3 leading-0 dark:text-font-white">
-                {translation(language, 'services.mark')}
-              </mark>
-            </h1>
-            <p className="my-6 lg:my-7 text-sm lg:text-base">
-              {translation(language, 'services.text')}
-            </p>
-            <Link
-              to={`/${language === 'es' ? '' : language + '/'}#contact`}
-              aria-label="Cotiza tu proyecto en servicios"
-              className="text-font-white bg-primary-400 hover:bg-primary-300 py-1.5 rounded-full active:bg-primary-500 px-8 md:px-10 lg:px-12 text-sm lg:text-base"
-            >
-              {translation(language, 'services.button')}
-            </Link>
-          </Fade>
-        </div>
-        <Fade triggerOnce>
-          <CardsStack services={services} />
+      <div className="w-10/12 md:w-4/12 mb-20 md:mb-0 mr-0 md:mr-20 xl:mr-28 -mt-6 sm:-mt-10 md:-mt-14 lg:-mt-20">
+        <Fade cascade triggerOnce>
+          <h1 className="text-lg md:text-xl xl:text-2xl font-bold">
+            {translation(language, 'services.first')}{' '}
+            <span className="text-primary-400">
+              {translation(language, 'services.span')}
+            </span>{' '}
+            {translation(language, 'services.second')}{' '}
+            <mark className="text-font0-black inline-block bg-primary-200 bg-opacity-50 pb-3 leading-0 dark:text-font-white">
+              {translation(language, 'services.mark')}
+            </mark>
+          </h1>
+          <p className="my-6 lg:my-7 text-sm lg:text-base">
+            {translation(language, 'services.text')}
+          </p>
+          <Link
+            to={`/${language === 'es' ? '' : language + '/'}#contact`}
+            aria-label="Cotiza tu proyecto en servicios"
+            className="text-font-white bg-primary-400 hover:bg-primary-300 py-1.5 rounded-full active:bg-primary-500 px-8 md:px-10 lg:px-12 text-sm lg:text-base"
+          >
+            {translation(language, 'services.button')}
+          </Link>
         </Fade>
-        <div className="absolute top-32 right-1/2 rounded-blob w-80 md:w-3/12 h-104 brick bg-medium-blue dark:bg-dark-gray bg-opacity-90 dark:bg-opacity-25" />
-      </section>
-    </Waypoint>
+      </div>
+      <Waypoint
+        onEnter={() => {
+          updateUrl(`/${language === 'es' ? '' : language + '/'}#services`);
+        }}
+      />
+      <Fade triggerOnce>
+        <CardsStack services={services} />
+      </Fade>
+      <div className="absolute top-32 right-1/2 rounded-blob w-80 md:w-3/12 h-104 brick bg-medium-blue dark:bg-dark-gray bg-opacity-90 dark:bg-opacity-25" />
+    </section>
   );
 }
