3b703c08923dafc1b154605915d6494c626e97e5
Erika Han <hanerika16@gmail.com>
2021-08-09 17:56:02 -0400

    deleted code

--- src/components/sdaas/ByTheNumbers.tsx
+++ src/components/sdaas/ByTheNumbers.tsx
@@ -81,60 +81,6 @@ export default function ByTheNumbers() {
     },
   ];
 
-  const roundCornersPlugin = {
-    // eslint-disable-next-line object-shorthand
-    afterUpdate: function (chart) {
-      if (chart.config?.options?.elements?.arc?.roundedCornersFor) {
-        const arc =
-          chart.getDatasetMeta(0).data[
-            chart.config.options.elements.arc.roundedCornersFor
-          ];
-        arc.round = {
-          x: (chart.chartArea.left + chart.chartArea.right) / 2,
-          y: (chart.chartArea.top + chart.chartArea.bottom) / 2,
-          radius: (chart.outerRadius + chart.innerRadius) / 2,
-          thickness: (chart.outerRadius - chart.innerRadius) / 2 - 1,
-          backgroundColor: arc._model.backgroundColor,
-        };
-      }
-    },
-
-    // eslint-disable-next-line object-shorthand
-    afterDraw: function (chart) {
-      if (chart.config?.options?.elements?.arc?.roundedCornersFor) {
-        const { ctx } = chart.chart;
-        const arc =
-          chart.getDatasetMeta(0).data[
-            chart.config.options.elements.arc.roundedCornersFor
-          ];
-        const startAngle = Math.PI / 2 - arc._view.startAngle;
-        const endAngle = Math.PI / 2 - arc._view.endAngle;
-
-        ctx.save();
-        ctx.translate(arc.round.x, arc.round.y);
-        ctx.fillStyle = arc.round.backgroundColor;
-        ctx.beginPath();
-        ctx.arc(
-          arc.round.radius * Math.sin(startAngle),
-          arc.round.radius * Math.cos(startAngle),
-          arc.round.thickness,
-          0,
-          2 * Math.PI
-        );
-        ctx.arc(
-          arc.round.radius * Math.sin(endAngle),
-          arc.round.radius * Math.cos(endAngle),
-          arc.round.thickness,
-          0,
-          2 * Math.PI
-        );
-        ctx.closePath();
-        ctx.fill();
-        ctx.restore();
-      }
-    },
-  };
-
   return (
     <section className="pb-36 px-14 md:px-20 lg:px-28 xl:px-44 relative z-10 overflow-hidden">
       <Fade duration={600} cascade triggerOnce>
