2090ce8c3d16262578608f23fdae1032d0d05c50
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2021-10-08 15:01:16 -0400

    hotjar

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  extends: ["avilatek-typescript"],
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: ['avilatek-typescript'],
+  rules: {
+    '@typescript-eslint/no-use-before-define': [1],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'prefer-arrow-callback': 'off',
+  },
 };
