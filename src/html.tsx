2a8f613360597dd2305970b6249ac6bd6397a6c7
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2021-10-20 11:34:10 -0400

    pixel

--- src/html.tsx
+++ src/html.tsx
@@ -35,6 +35,31 @@ export default function HTML({ ...props }: HTMLProps) {
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '575577650313546');
+          fbq('track', 'PageView');
+        `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=575577650313546&ev=PageView&noscript=1"
+            alt="fb pixel"
+          />
+        </noscript>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
