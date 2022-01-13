cc4b5750b2523348c14bc42cbe2d24ca175050ec
Erika Han <hanerika16@gmail.com>
2022-01-12 22:02:15 -0400

    social sharing

--- src/templates/PostProfile.tsx
+++ src/templates/PostProfile.tsx
@@ -7,26 +7,33 @@ import RotatingTriangle from '../components/icons/RotatingTriangle';
 import Line from '../components/icons/Line';
 import SEO from '../components/SEO';
 import Subscription from '../components/common/Subscription';
-import Image from '../components/common/Image';
+import SocialSharing from './SocialSharing';
 
 dayjs.locale('es');
 
 export default function SinglePostPage({ data }) {
+  const SITE_URL = 'https://avilatek.com/';
   const { frontmatter, html } = data.markdownRemark;
   const {
+    slug,
     author,
     authorDescription,
-    // authorImage,
+    coauthor,
     category,
     date,
-    image,
     subtitle,
     title,
+    description,
   } = frontmatter;
 
   return (
     <>
-      <SEO title={title} />
+      <SEO
+        title={`${title} | Avila Tek`}
+        name={`${title} | Avila Tek`}
+        description={description}
+        link={`${SITE_URL}blog/${slug}`}
+      />
       <main>
         {/* Banner */}
         <section className="relative w-full bg-light-blue dark:bg-dark-gray flex flex-col items-center pt-32 md:pt-44 transition duration-300 ease-in-out">
@@ -49,6 +56,7 @@ export default function SinglePostPage({ data }) {
           <Waves className="w-full h-full" />
           <div className="bg-light dark:bg-dark w-full h-6 -mt-3" />
         </div>
+
         {/* Post */}
         <section className="w-10/12 sm:w-9/12 md:w-7/12 mx-auto mt-8 md:mt-0 lg:-mt-10 xl:-mt-24 mb-40">
           {/* Post header */}
@@ -56,54 +64,40 @@ export default function SinglePostPage({ data }) {
             <h2 className="font-semibold text-2base md:text-lg xl:text-xl text-secondary-100 dark:text-neutral-100">
               {subtitle}
             </h2>
-            <div className="flex items-center mt-3">
-              {/* {authorImage ? (
-                <Image
-                  filename={authorImage}
-                  alt={author}
-                  className="h-8 w-8 md:h-9 md:w-9 lg:h-10 lg:w-10 bg-primary-400 rounded-full overflow-hidden mr-3"
-                />
-              ) : null} */}
-              <p className="text-xsm sm:text-sm xl:text-base text-neutral-400 font-medium">
-                Por{' '}
-                <span className="text-primary-400 dark:text-primary-300 mr-3">
-                  {author}
-                </span>
-                |
-                <span className="ml-3">
+            <div className="flex items-center justify-between mt-3">
+              <div className="flex flex-col sm:flex-row sm:items-center text-xsm sm:text-sm xl:text-base text-neutral-400 font-medium">
+                <p>
+                  Por{' '}
+                  <span className="text-primary-400 dark:text-primary-300">
+                    {author}
+                    {coauthor && <>, {coauthor}</>}
+                  </span>{' '}
+                </p>
+                <span className="mx-3 hidden sm:inline-block">|</span>
+                <p>
                   {dayjs(new Date(date).toISOString().split('T')[0]).format(
                     'DD MMM, YYYY'
                   )}
-                </span>
-              </p>
+                </p>
+              </div>
+              {/* Share buttons */}
             </div>
+            <SocialSharing SITE_URL={SITE_URL} slug={slug} title={title} />
           </div>
-          <hr className="my-12 border border-medium-blue dark:border-dark-gray" />
+          <hr className="mt-6 mb-12  border border-medium-blue dark:border-dark-gray" />
           {/* Post body */}
           <div className="w-full grid gap-6 text-sm lg:text-base">
-            {/* {image ? (
-              <Image
-                filename={image}
-                alt={title}
-                className="w-full h-half lg:h-third mb-4"
-              />
-            ) : null} */}
             <div
               className="w-full grid gap-5 text-2sm lg:text-base break-words text-justify leading-normal lg:leading-normal"
               // eslint-disable-next-line react/no-danger
               dangerouslySetInnerHTML={{ __html: html }}
             />
           </div>
-          <hr className="my-12 border border-medium-blue dark:border-dark-gray" />
+
+          <SocialSharing SITE_URL={SITE_URL} slug={slug} title={title} footer />
+          <hr className="mt-4 mb-12 border border-medium-blue dark:border-dark-gray" />
           {/* Post footer */}
           <div className="flex flex-col md:flex-row">
-            {/* {authorImage ? (
-              <Image
-                filename={authorImage}
-                alt={author}
-                className="w-14 h-14 sm:w-16 sm:h-16 lg:h-20 lg:w-20 mr-6 mb-4 bg-primary-400 rounded-full overflow-hidden"
-              />
-            ) : null} */}
             <div className="w-11/12 md:w-9/12">
               <h3 className="capitalize font-semibold text-primary-400 dark:text-primary-300 text-sm lg:text-base xl:text-2base">
                 {author}
@@ -127,11 +121,12 @@ export const postQuery = graphql`
     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       html
       frontmatter {
+        slug
         title
         subtitle
         date
         author
-        # authorImage
+        coauthor
         description
         image
         authorDescription
